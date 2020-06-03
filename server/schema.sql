f5925d3824af086c467ae265c448d387ba142f04
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-03 16:10:07 -0700

    Updates Client Side and Builds Tables for DB
    This commit loads all of the client side files for the chatterbox
    program.
    
    Also we designed and added three tables that will be created within our
    database.

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  id integer auto_increment primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE users (
+  id integer auto_increment primary key,
+  name VARCHAR(255)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id integer auto_increment primary key,
+  messages varchar(255),
+  users_id integer,
+  rooms_id integer,
+  foreign key(users_id) references users(id),
+  foreign key(rooms_id) references rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
 
 
 
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
